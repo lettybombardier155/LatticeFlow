9f496c42dcbcacf1a04cbde092613c008ec111e7
lettybombardier155 <129162807+lettybombardier155@users.noreply.github.com>
2025-10-12 10:13:49 +0500

    Create __init__.py

---
+++ training/__init__.py
@@ -0,0 +1,6 @@
+"""
+LatticeFlow Training Package
+----------------------------
+Includes dataset utilities, distributed trainer, and experiment orchestration.
+"""
+__all__ = ["DatasetLoader", "DistributedTrainer", "ExperimentManager"]
